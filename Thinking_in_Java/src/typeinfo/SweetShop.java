29ead11ef9de22c4c7beb024842bc3b1a48fc604
叶青 <yeqing_613@163.com>
2020-09-23 18:22:02 +0800

    docs(typeinfo/):add directory typeinfo/ and file typeinfo/toys/ToyTest.java

---
+++ Thinking_in_Java/src/typeinfo/SweetShop.java
@@ -0,0 +1,34 @@
+package typeinfo;
+
+import static net.mindview.util.Print.*;
+
+class Candy {
+	static {print("Loading Candy");}
+}
+
+class Gum {
+	static { print("Loading Gum"); }
+}
+
+class Cookie {
+	static {print("Loading Cookie");}
+}
+
+public class SweetShop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        print("inside main");
+        new Candy();
+        print("After creating Candy");
+        try {
+        	Class.forName("Gum");
+        } catch(ClassNotFoundException e) {
+        	print("Couldn't find Gum");
+        }
+        print("After Class.forName(\"Gum\")");
+        new Cookie();
+        print("After creating Cookie");
+	}
+
+}
