8141e5cdcdd06dfbca70380251ea88423c6e0b34
叶青 <yeqing_613@163.com>
2020-10-03 23:18:34 +0800

    docs(typeinfo/):modified Ex19_ToyTest2.java

--- Thinking_in_Java/src/typeinfo/Ex19_ToyTest2.java
+++ Thinking_in_Java/src/typeinfo/Ex19_ToyTest2.java
@@ -28,43 +28,7 @@ public class Ex19_ToyTest2 {
     	print("Is this class an interface? [" + c.isInterface() + "]");
     }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        Class<?> c = null;
-		try {
-        	c = Class.forName("typeinfo.toys.FancyToy");
-        } catch(ClassNotFoundException e) {
-        	print("Couldn't found \"FancyToy\"");
-        }
-	 	printInfo(c);
-    	print("Super class: " + c.getSuperclass());
-    	Class<?>[] interfaces = c.getInterfaces();
-    	print("Its all interfaces of implemented: ");
-    	for(Class<?> i : interfaces) {
-    		printInfo(i);
-    	}
-    	print();
-    	print("Call newInstance(): ");
-    	Class<?> up = c.getSuperclass();
-    	Constructor<?>[] ctors = c.getConstructors();
-    	System.out.println(ctors.length);
-    	Object obj = null;
-		try {
-			obj = ctors[0].newInstance();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	printInfo(obj.getClass());
+		
 	}
 
 }
