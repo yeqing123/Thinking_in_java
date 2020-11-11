3c8223cb8cf334cd8e3fcf7f306f2d9a8e9f8e35
叶青 <yeqing_613@163.com>
2020-11-11 22:45:44 +0800

    modify(containers/):modified SortedSetTest.java, done

--- Thinking_in_Java/src/containers/SortedSetTest.java
+++ Thinking_in_Java/src/containers/SortedSetTest.java
@@ -6,8 +6,8 @@ import net.mindview.util.RandomGenerator;
 
 // 让元素自身具有比较性，对这种元素集合进行的排序，叫做“自然排序”
 class Ordered implements Comparable<Object>  {
-    private int i;
-    public Ordered(int i) {
+    private Integer i;
+    public Ordered(Integer i) {
     	this.i = i;
     }
     public int getInt() { return i; }
@@ -26,8 +26,8 @@ class Ordered implements Comparable<Object>  {
 
 // 自身不具备比较性的元素，如果要排序需要自己定制比较器
 class Unordered {
-	private int i;
-	public Unordered(int i) { this.i = i; }
+	private Integer i;
+	public Unordered(Integer i) { this.i = i; }
 	public int getInt() { return i; }
 	public String toString() { return String.valueOf(i); }
 }
@@ -46,33 +46,37 @@ class CustomComparator implements Comparator<Unordered> {
 
 public class SortedSetTest {
 	static RandomGenerator.Integer gen = new RandomGenerator.Integer(100);
-    public static <T> void fill(Collection<?> c, int quantity, Class<?> type) {
+    public static <T> void fill(Collection<T> c, Class<T> type, int quantity) throws Exception {
     	Constructor<T> con = type.getDeclaredConstructor(Integer.class);
     	for(int i = 0; i < quantity; i++)
     		c.add(con.newInstance(gen.next()));
     }
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws Exception {
 		// TODO Auto-generated method stub
         // List集合，其自身不具备排序性质
 		List<Ordered> list = new LinkedList<Ordered>();
-        RandomGenerator.Integer gen = new RandomGenerator.Integer(100);
-        for(int i = 0; i < 10; i++)
-        	list.add(new Ordered(gen.next()));
+		// 填充集合元素
+        fill(list, Ordered.class, 10);
         // 因为Ordered自身具备比较性，因此无需额外的比较器即可进行排序
         Collections.sort(list);
         System.out.println(list);
         
         List<Unordered> list2 = new LinkedList<Unordered>();
-        for(int i = 0; i < 10; i++)
-        	list2.add(new Unordered(gen.next()));
+        fill(list2, Unordered.class, 10);
         // 因为Unordered类本身不具备比较性，因此需要加入额外的比较器
         Collections.sort(list2, new CustomComparator());
         System.out.println(list2);
         // TreeSet集合，其自身具备排序性质
         TreeSet<Ordered> set1 = new TreeSet<Ordered>();
-        
-        TreeSet<Unordered> set1 = new TreeSet<Unordered>(new CustomComparator());
-        System.out.println(set.comparator().toString());
+        fill(set1, Ordered.class, 10);
+        System.out.println(set1);
+        // comparator()会返回null，因为使用的是“自然排序”，没有额外的比较器
+        System.out.println(set1.comparator());
+        // 因为Unordered不具备自比较能力，因此创建TreeSet集合的同时需要向其传递一个额外的比较器
+        TreeSet<Unordered> set2 = new TreeSet<Unordered>(new CustomComparator());
+        fill(set2, Unordered.class, 10);
+        System.out.println(set2);
+        System.out.println(set2.comparator());
 	}
 
 }
