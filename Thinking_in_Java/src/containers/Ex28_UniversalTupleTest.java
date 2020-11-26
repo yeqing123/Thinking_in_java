3221f2b2820160f30981e46e89c49cf5d869097b
叶青 <yeqing_613@163.com>
2020-11-26 16:45:08 +0800

    fix(containers/):fix /Ex28_UniversalTupleTest.java

--- Thinking_in_Java/src/containers/Ex28_UniversalTupleTest.java
+++ Thinking_in_Java/src/containers/Ex28_UniversalTupleTest.java
@@ -1,140 +1,189 @@
 package containers;
-import java.util.*;
+import java.util.Set;
+import java.util.TreeSet;
 
+import containers.Tuple.*;
 
-interface Tuple {}
-
-class TwoTuple<A, B> implements Tuple, Comparable<Tuple> {
-    private static long count = 0;
-    private long id = count++;
-	public final A first;
-	public final B second;
-	public TwoTuple(A first, B second) {
-		this.first = first;
-		this.second = second;
+class Tuple {
+	public static class T2<A, B> implements Comparable<T2<A, B>> {
+		private final A first;
+		private final B second;
+		public T2(A a, B b) {
+			first = a;
+			second = b;
+		}
+		public String toString() {
+			return "(" + first + ", " + second + ")";
+		}
+		public A getFirst() { return first; }
+        public B getSecond() { return second; }
+		public int hashCode() {
+			int result = 17;
+			result = result * 37 + first.hashCode();
+			result = result * 37 + second.hashCode();
+			return result;
+		}
+		@SuppressWarnings("unchecked")
+		public boolean equals(Object o) {
+			if(!(o instanceof T2))
+				return false;
+			T2<A, B> t = (T2<A, B>)o;
+			return first == null ? t.getFirst() == null : t.getFirst().equals(first) &&
+			       second == null ? t.getSecond() == null : t.getSecond().equals(second);		
+		}
+		@SuppressWarnings("unchecked")
+		@Override
+		public int compareTo(T2<A, B> arg) {
+			int firstCompare = ((Comparable<A>)first).compareTo(arg.getFirst());
+			if(firstCompare != 0)
+				return firstCompare;
+			return ((Comparable<B>)second).compareTo(arg.getSecond());
+		}
 	}
-	public String toString() {
-		return "(" + first + ", " + second + ")";
+	public static class T3<A, B, C> implements Comparable<T3<A, B, C>> {
+        private final T2<A, B> t2;
+        private final C third;
+        public T3(A a, B b, C c) {
+        	t2 = new T2<A, B>(a, b);
+        	third = c;
+        }
+        public A getFirst() { return t2.getFirst(); }
+        public B getSecond() { return t2.getSecond(); }
+        public C getThird() { return third; }
+        public String toString() {
+        	return "(" + t2.getFirst() + ", " + t2.getSecond() + ", " + third + ")";
+        }
+        public int hashCode() {
+        	return t2.hashCode() * 37 + third.hashCode();
+        }
+        @SuppressWarnings("unchecked")
+		public boolean equals(Object o) {
+        	if(!(o instanceof T3))
+        		return false;
+        	T3<A, B, C> t = (T3<A, B, C>)o;
+        	return t2.equals(new T2<A, B>(t.getFirst(), t.getSecond())) &&
+        			third == null ? t.getThird() == null : third.equals(t.getThird());
+        }
+		@SuppressWarnings("unchecked")
+		@Override
+		public int compareTo(T3<A, B, C> arg) {
+			int t2Compare = t2.compareTo(new T2<A, B>(arg.getFirst(), arg.getSecond()));
+			if(t2Compare != 0)
+				return t2Compare;
+			return ((Comparable<C>)third).compareTo(arg.getThird());
+		}
 	}
-	public int hashCode() {
-		int result = 17;
-		result = result * 37 + first.hashCode();
-		result = result * 37 + second.hashCode();
-		return result;
+	public static class T4<A, B, C, D> implements Comparable<T4<A, B, C, D>> {
+        private final T3<A, B, C> t3;
+        private final D fourth;
+        public T4(A a, B b, C c, D d) {
+        	t3 = new T3<A, B, C>(a, b, c);
+        	fourth = d;
+        }
+        public A getFirst() { return t3.getFirst(); }
+        public B getSecond() { return t3.getSecond(); }
+        public C getThird() { return t3.getThird(); }
+        public D getFourth() { return fourth; }
+        public String toString() {
+        	return "(" + t3.getFirst() + ", " + t3.getSecond() + 
+        			", " + t3.getThird() + ", " + fourth + ")";
+        }
+        public int hashCode() {
+        	return t3.hashCode() *37 + fourth.hashCode();
+        }
+        @SuppressWarnings("unchecked")
+		public boolean equals(Object o) {
+        	if(!(o instanceof T4))
+        		return false;
+        	T4<A, B, C, D> t = (T4<A, B, C, D>)o;
+        	return t3.equals(new T3<A, B, C>(t.getFirst(), t.getSecond(), t.getThird())) && 
+        			fourth == null ? t.getFourth() == null : fourth.equals(t.getFourth());
+        }
+		@SuppressWarnings("unchecked")
+		@Override
+		public int compareTo(T4<A, B, C, D> arg) {
+            int t3Compare = t3.compareTo(new T3<A, B, C>(arg.getFirst(), arg.getSecond(), arg.getThird()));
+            if(t3Compare != 0)
+            	return t3Compare;
+			return ((Comparable<D>)fourth).compareTo(arg.getFourth());
+		}
 	}
-	@SuppressWarnings("rawtypes")
-	public boolean equals(Object o) {
-		return o instanceof TwoTuple &&
-				((TwoTuple)o).first.equals(first) &&
-		        ((TwoTuple)o).second.equals(second);		
+	public static class T5<A, B, C, D, E> implements Comparable<T5<A, B, C, D, E>> {
+        private final T4<A, B, C, D> t4;
+        private final E fifth;
+        public T5(A a, B b, C c, D d, E e) {
+        	t4 = new T4<A, B, C, D>(a, b, c, d);
+        	fifth = e;
+        }
+        public A getFirst() { return t4.getFirst(); }
+        public B getSecond() { return t4.getSecond(); }
+        public C getThird() { return t4.getThird(); }
+        public D getFourth() { return t4.getFourth(); }
+        public E getFifth() { return fifth; }
+        public String toString() {
+        	return "(" + t4.getFirst() + ", " + t4.getSecond() + ", " + 
+                t4.getThird() + ", " + t4.getFourth() + ", " + fifth + ")";
+        }
+        public int hashCode() {
+        	return t4.hashCode() * 37 + fifth.hashCode();
+        }
+        @SuppressWarnings("unchecked")
+		public boolean equals(Object o) {
+        	if(!(o instanceof T5))
+        		return false;
+        	T5<A, B, C, D, E> t = (T5<A, B, C, D, E>)o;
+        	return t4.equals(new T4<A, B, C, D>(t.getFirst(), t.getSecond(), t.getThird(), t.getFourth())) &&
+        			fifth == null ? t.getFifth() == null : fifth.equals(t.getFifth());
+        }
+		@SuppressWarnings("unchecked")
+		@Override
+		public int compareTo(T5<A, B, C, D, E> arg) {
+			int t4Compare = t4.compareTo(
+					new T4<A, B, C, D>(arg.getFirst(), arg.getSecond(), arg.getThird(), arg.getFourth()));
+			if(t4Compare != 0)
+				return t4Compare;
+			return ((Comparable<E>)fifth).compareTo(arg.getFifth());
+		}
 	}
-	@Override
-	public int compareTo(Tuple arg) {
-		String firstName = first.getClass().getSimpleName();
-		String argFirstName = ((TwoTuple)arg).getClass().getSimpleName();
-		int firstCompare = firstName.compareTo(argFirstName);
-		if(firstCompare != 0)
-			return firstCompare;
-		String secondName = second.getClass().getSimpleName();
-		String argSecondName = ((TwoTuple)arg).second.getClass().getSimpleName();
-	    int secondCompare = secondName.compareTo(argSecondName);
-	    if(secondCompare != 0)
-	    	return secondCompare;
-		return id > ((TwoTuple)arg).id ? 1 : (id == ((TwoTuple)arg).id ? 0 : -1);
+	public static <A, B> T2<A, B> tuple(A a, B b) {
+		return new T2<A, B>(a, b);
+	}
+	public static <A, B, C> T3<A, B, C> tuple(A a, B b, C c) {
+		return new T3<A, B, C>(a, b, c);
+	}
+	public static <A, B, C, D> T4<A, B, C, D> tuple(A a, B b, C c, D d) {
+		return new T4<A, B, C, D>(a, b, c, d);
+	}
+	public static <A, B, C, D, E> T5<A, B, C, D, E> tuple(A a, B b, C c, D d, E e) {
+		return new T5<A, B, C, D, E>(a, b, c, d, e);
 	}
 }
 
-class ThreeTuple<A, B, C> extends TwoTuple<A, B> implements Comparable<Tuple> {
-    private static long count = 0;
-    private long id = count++;
-	public final C three;
-    public ThreeTuple(A first, B second, C three) {
-		super(first, second);
-        this.three = three;
-	}
-	public String toString() {
-		return "(" + first + ", " + second + ", " + three + ")";
-	}
-	public int hashCode() {
-		return super.hashCode() * 37 + three.hashCode();
-	}
-	public boolean equals(Object o) {
-		return o instanceof ThreeTuple && super.equals(o);
-	}
-	@Override
-	public int compareTo(Tuple arg) {
-		int superCompare = super.compareTo(arg);
-		if(superCompare != 0)
-			return superCompare;
-		String threeName = three.getClass().getSimpleName();
-		String argThreeName = ((ThreeTuple)arg).three.getClass().getSimpleName();
-		int threeCompare = threeName.compareTo(argThreeName);
-		if(threeCompare != 0)
-			return threeCompare;
-		return id > ((ThreeTuple)arg).id ? 1 : (id == ((ThreeTuple)arg).id ? 0 : -1);
-	}
-}
 
-class FourTuple<A, B, C, D> extends ThreeTuple<A, B, C> {
-	private static long count = 0;
-	private long id = count++;
-	public D four;
-	public FourTuple(A first, B second, C three, D four) {
-		super(first, second, three);
-        this.four = four;
-	}
-	public String toString() {
-		return "(" + first + ", " + second + ", " + three + ", " + four + ")";
-	}
-	public int hashCode() {
-		return super.hashCode() + 37 + four.hashCode();
-	}
-	public boolean equals(Object o) {
-		return o instanceof ThreeTuple && super.equals(o);
-	}
-//	@Override
-//	public int compareTo(Tuple arg) {
-//		int superCompare = super.compareTo(arg);
-//		if(superCompare != 0)
-//			return superCompare;
-//		String fourName = four.getClass().getSimpleName();
-//		String argFourName = ((FourTuple)arg).four.getClass().getSimpleName();
-//		int fourCompare = fourName.compareTo(argFourName);
-//		if(fourCompare != 0)
-//			return fourCompare;
-//		return id > ((FourTuple)arg).id ? 1 : (id == ((FourTuple)arg).id ? 0 : -1);
-//	}
-}
 
 public class Ex28_UniversalTupleTest {
-	@SuppressWarnings({ "null", "unchecked" })
+ 
 	public static void main(String[] args) {
-//        Map<Tuple, Integer> map = new HashMap<Tuple, Integer>();
-//        TwoTuple<String, String>[] twoTuples = new TwoTuple[5];
-//        ThreeTuple<String, String, Integer>[] threeTuples = new ThreeTuple[5];
-//        FourTuple<String, String, Integer, Byte>[] fourTuples = new FourTuple[5];
-//        Random rand = new Random(47);
-//        byte[] bytes = null;
-//        rand.nextBytes(bytes);
-//        String[] array = "A B C D E".split(" ");
-//        for(int i = 0; i < twoTuples.length; i++) {
-//        	twoTuples[i] = new TwoTuple<String, String>("hi", "ye");
-//        	map.put(twoTuples[i], i);
-//        }
-//        for(int i = 0; i < threeTuples.length; i++) {
-//        	threeTuples[i] = new ThreeTuple<String, String, Integer>(
-//        			"hi", "ye", rand.nextInt(100));
-//        	map.put(threeTuples[i], i);
-//        }
-//        for(int i = 0; i < array.length; i++)
-//        	bytes[i] = Byte.valueOf(array[i]);
-//        for(int i = 0; i < fourTuples.length; i++) {
-//        	fourTuples[i] = new FourTuple<String, String, Integer, Byte>(
-//        			"hi", "ye", rand.nextInt(100), bytes[rand.nextInt(bytes.length)]);
-//        	map.put(fourTuples[i], i);
-//        }
-        System.out.println(new TwoTuple<String, String>("hi", "ye"));
-        System.out.println(new ThreeTuple<String, String, Integer>("hi", "ye", 10));
+		Set<T2<?, ?>> t2Set = new TreeSet<T2<?, ?>>();
+		Set<T3<?, ?, ?>> t3Set = new TreeSet<T3<?, ?, ?>>();
+        T2<String, Integer> t2_1 =  Tuple.tuple("C", 10);
+        T2<String, Integer> t2_2 =  Tuple.tuple("B", 8);
+        T2<String, Integer> t2_3 =  Tuple.tuple("A", 14);
+        T2<String, Integer> t2_4 =  Tuple.tuple("B", 9);
+        T3<String, Integer, Character> t3_1 = Tuple.tuple("D", 17, 'c');
+        T3<String, Integer, Character> t3_2 = Tuple.tuple("F", 19, 'b');
+        T3<String, Integer, Character> t3_3 = Tuple.tuple("E", 15, 'd');
+        T3<String, Integer, Character> t3_4 = Tuple.tuple("F", 19, 'e');
+        t2Set.add(t2_1);
+        t2Set.add(t2_2);
+        t2Set.add(t2_3);
+        t2Set.add(t2_4);
+        t3Set.add(t3_1);
+        t3Set.add(t3_2);
+        t3Set.add(t3_3);
+        t3Set.add(t3_4);
+        System.out.println(t2Set);
+        System.out.println(t3Set);
 	}
 
 }
