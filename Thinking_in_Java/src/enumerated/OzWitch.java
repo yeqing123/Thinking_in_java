cef248370cfc02656a42e363a5bfc65df6d8a763
叶青 <yeqing_613@163.com>
2021-01-05 19:57:58 +0800

    feat(src/): Add an enumerated/ packge, and five java files

---
+++ Thinking_in_Java/src/enumerated/OzWitch.java
@@ -0,0 +1,21 @@
+// The witches in the lang of OZ.
+package enumerated;
+
+public enum OzWitch {
+    // Instances must be defined first, before methods:
+	WEST("Miss Gulch, aka the Witched Witch of the West"),
+	NORTH("Wicked, the Good Witch of the North"),
+	EAST("Wiched Witch of the East, wearer of the Ruby" + 
+	    "Slippers, crushed by Dorothy's house"),
+	SOUTH("Good by inference, but missing");
+	private String description;
+	// Constructor must be package or private access:
+	private OzWitch(String description) {
+		this.description = description;
+	}
+	public String getDescription() { return description; }
+	public static void main(String[] args) {
+		for(OzWitch witch : OzWitch.values())
+			System.out.println(witch + ": " + witch.getDescription());
+	}
+}
