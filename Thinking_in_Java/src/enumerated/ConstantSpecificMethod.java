637814ad4a5dd6efebf999d55f5a1882d8d45ca9
叶青 <yeqing_613@163.com>
2021-01-08 17:53:19 +0800

    feat(enumerated/): Add ConstantSpecificMethod, EnumMaps, NotClasses three java files

---
+++ Thinking_in_Java/src/enumerated/ConstantSpecificMethod.java
@@ -0,0 +1,27 @@
+package enumerated;
+import java.text.*;
+import java.util.*;
+
+public enum ConstantSpecificMethod {
+    DATE_TIME {
+    	String getInfo() {
+    		return DateFormat.getDateInstance().format(new Date());
+    	}
+    },
+    CLASSPATH {
+    	String getInfo() {
+    		return System.getenv("CLASSPATH");
+    	}
+    },
+    VERSION {
+    	String getInfo() {
+    		return System.getProperty("java.version");
+    	}
+    };
+	abstract String getInfo();
+	public static void main(String[] args) {
+        for(ConstantSpecificMethod csm : values())
+        	System.out.println(csm.getInfo());
+	}
+
+}
---
+++ Thinking_in_Java/src/enumerated/NotClasses.java
@@ -0,0 +1,13 @@
+// {Exec: javap -c LikeClasses}
+package enumerated;
+
+enum LikeClasses {
+	WINKEN { void behavior() { System.out.println("Behavior1"); } },
+	BLINKEN { void behavior() { System.out.println("Behavior2"); } },
+	NOD { void behavior() { System.out.println("Behavior3"); } };
+	abstract void behavior();
+}
+
+public class NotClasses {
+    // void f1(LikeClasses.WINKEN instance) {} // Nope
+}
