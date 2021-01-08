670967817e7abde12e7bd09841d25af76121fb78
叶青 <yeqing_613@163.com>
2021-01-08 15:40:52 +0800

    feat(enumerated/): Add AlarmPoints, BigEnumSet, EnumSets three java files

---
+++ Thinking_in_Java/src/enumerated/EnumSets.java
@@ -0,0 +1,21 @@
+package enumerated;
+import java.util.*;
+import static enumerated.AlarmPoints.*;
+
+public class EnumSets {
+	public static void main(String[] args) {
+        EnumSet<AlarmPoints> points = 
+        		EnumSet.noneOf(AlarmPoints.class);  // Empty set
+        points.add(BATHROOM);
+        System.out.println(points);
+        points.addAll(EnumSet.of(STAIR1, STAIR2, KITCHEN));
+        System.out.println(points);
+        points = EnumSet.allOf(AlarmPoints.class);
+        points.removeAll(EnumSet.of(STAIR1, STAIR2, KITCHEN));
+        System.out.println(points);
+        points.removeAll(EnumSet.range(OFFICE1, OFFICE4));
+        System.out.println(points);
+        points = EnumSet.complementOf(points);
+        System.out.println(points);
+	}
+}
