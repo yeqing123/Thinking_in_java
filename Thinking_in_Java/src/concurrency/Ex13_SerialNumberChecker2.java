3f2eeb84286d9efabbacae8572b42f1c47279ea4
叶青 <yeqing_613@163.com>
2021-02-07 23:13:09 +0800

    feat(concurrency/): modify Ex12, Ex13

--- Thinking_in_Java/src/concurrency/Ex13_SerialNumberChecker2.java
+++ Thinking_in_Java/src/concurrency/Ex13_SerialNumberChecker2.java
@@ -1,9 +1,40 @@
+// {Args: 4}
 package concurrency;
+import java.util.concurrent.*;
+
+class SerialNumberGenerator2 {
+	private static int serialNumber;
+	public synchronized static int nextSerialNumber() {
+		return serialNumber++;
+	}
+}
 
 public class Ex13_SerialNumberChecker2 {
-
+    private static final int SIZE = 10;
+    private static CircularSet serials = 
+    		new CircularSet(1000);
+    private static ExecutorService exec = Executors.newCachedThreadPool();
+    static class SerialChecker implements Runnable {
+    	public void run() {
+    		int serial = SerialNumberGenerator2.nextSerialNumber();
+    		if(serials.contains(serial)) {
+    			System.out.println("Duplicate: " + serial);
+    			System.exit(0);
+    		}
+    		serials.add(serial);
+    	}
+    }
 	public static void main(String[] args) throws Exception {
-		SerialNumberChecker.main(args);
+        for(int i = 0; i < SIZE; i++)
+        	exec.execute(new SerialChecker());
+        if(args.length > 0) {
+        	TimeUnit.SECONDS.sleep(new Integer(args[0]));
+        	System.out.println("No duplicates detected");
+        	System.exit(0);
+        } else {
+        	System.err.println("Provide a sleep time in sec.");
+        	System.exit(1);
+        }
 	}
 
 }
--- Thinking_in_Java/src/concurrency/SerialNumberGenerator.java
+++ Thinking_in_Java/src/concurrency/SerialNumberGenerator.java
@@ -2,7 +2,7 @@ package concurrency;
 
 public class SerialNumberGenerator {
     private static volatile int serialNumber = 0;
-    public synchronized static int nextSerialNumber() {
+    public static int nextSerialNumber() {
     	return serialNumber++;
     }
 }
